c5c0a6ff4ca7d8d210922f284cc896334a24f352
dr4g0nB <ellak.arabrab@gmail.com>
2021-08-08 01:39:16 -0500

    Function to calculate the descuento of a product

---
+++ descuentos.js
@@ -0,0 +1,53 @@
+function calcularDescuento(precio, descuento) {
+  const porcentajePrecioConDescuento = 100 - descuento;
+  const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+  return precioConDescuento;
+}
+
+//Se le esta enviando un objeto al console.log
+//console.log({
+//  precioOriginal,
+//  descuento,
+//  porcentajePrecioConDescuento,
+//  precioConDescuento,
+//});
+
+//Codigo de Aidan Lorenzo
+class Product {
+  constructor(name, price, discount, country) {
+    this.name = name
+    this.price = price
+    this.discount = discount
+    this.country = country
+  }
+
+  computeDiscount() {
+    return this.computeTaxes(this.price) - (this.discount/100) * this.computeTaxes(this.price)
+  }
+
+  computeTaxes() {
+    return this.price + (this.taxesOfCountry(this.country) / 100) * this.price
+  }
+  
+  taxesOfCountry() {
+    //economipedia.com/ranking/el-iva-en-el-mundo.html
+    //utilizo el IVA reducido de los paises donde esta empresa tiene tiendas
+
+    switch (this.country.toLowerCase()) {
+      case "spain":
+        return 10
+      case "america":
+        return 5.5
+      case "brazil":
+        return 12
+      case "portugal":
+        return 13
+    }
+  }
+
+  computeRealPrice() {
+    return `The real price of your ${this.name} is ${this.computeDiscount().toFixed(2)}`
+  }
+
+}
