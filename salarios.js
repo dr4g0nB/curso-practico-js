8229b5c2c073c2f26eb7fcbec903abeabd3bb416
dr4g0nB <ellak.arabrab@gmail.com>
2021-08-12 17:51:44 -0500

    Analize the salary depending on the country

---
+++ salarios.js
@@ -0,0 +1,86 @@
+const colombia = []
+
+colombia.push({
+  name: 'Natalia',
+  salary: 1000
+})
+colombia.push({
+  name: 'Alberto',
+  salary: 1300
+})
+colombia.push({
+  name: 'Geraldine',
+  salary: 1850
+})
+colombia.push({
+  name: 'Alfredo',
+  salary: 2000
+})
+colombia.push({
+  name: 'Bella',
+  salary: 1050
+})
+colombia.push({
+  name: 'Mateo',
+  salary: 1540
+})
+colombia.push({
+  name: 'Luisa',
+  salary: 1540
+})
+colombia.push({
+  name: 'Esteban',
+  salary: 1640
+})
+colombia.push({
+  name: 'Simona',
+  salary: 1600
+})
+colombia.push({
+  name: 'Nath',
+  salary: 2200
+})
+colombia.push({
+  name: 'Carlos',
+  salary: 1500
+})
+colombia.push({
+  name: 'Ana',
+  salary: 2100
+})
+colombia.push({
+  name: 'Camilo',
+  salary: 2000
+})
+colombia.push({
+  name: 'Marcos',
+  salary: 1800
+})
+colombia.push({
+  name: 'Isabel',
+  salary: 1500
+})
+colombia.push({
+  name: 'Antonio',
+  salary: 1450
+})
+colombia.push({
+  name: 'Alma',
+  salary: 2000
+})
+colombia.push({
+  name: 'Colon',
+  salary: 2100
+})
+colombia.push({
+  name: 'Gigi',
+  salary: 1000
+})
+colombia.push({
+  name: 'David',
+  salary: 1000
+})
+colombia.push({
+  name: 'Jazmin',
+  salary: 1000
+})
